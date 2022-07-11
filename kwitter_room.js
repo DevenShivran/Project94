e39217acfc3b17f799e10b17b9551565874a72ab
DevenShivran <98974858+DevenShivran@users.noreply.github.com>
2022-07-11 16:08:56 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+    apiKey: "AIzaSyAQZXrVH1Fxo2g-P3vB8RPAEM-Wy03JGH4",
+    authDomain: "project94-6d5e9.firebaseapp.com",
+    databaseURL: "https://project94-6d5e9-default-rtdb.firebaseio.com",
+    projectId: "project94-6d5e9",
+    storageBucket: "project94-6d5e9.appspot.com",
+    messagingSenderId: "666831918995",
+    appId: "1:666831918995:web:c42987a1f1604068e53a66"
+  };
+firebase.initializeApp(firebaseConfig);
